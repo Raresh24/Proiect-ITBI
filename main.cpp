515c0199f816f2bdd01e5f5d5065b6e308b48fd8
Raresh24 <rares-mihai.georgescu@s.unibuc.ro>
2026-01-04 18:28:13 +0200

    Add files via upload
    Program de indentare

--- main.cpp
+++ main.cpp
@@ -1,22 +1,44 @@
-#include<iostream>
+#include <iostream>
 #include<fstream>
 #include<cstring>
 using namespace std;
+ifstream f("file.in");
+ofstream c("file.out");
+void printspace( int niv ){
+for (int i=0; i<niv *4;i++)
+    c << " ";
+}
 int main()
 {
-    ifstream fin("file.in");
-    ofstream fout("file.out");
-    char linie[256],*cuv;
-    while(fin.getline(linie,256))
-    {
-        cuv=strtok(linie,">");
-        while(cuv!=NULL)
-        {
-            fout<<cuv<<">"<<endl;
-            cuv=strtok(NULL,">");
-        }
+char s[1000];
+int niv =0;
+while ( f.getline(s,1000)) {
+    if (s[0] == '<' && s[1]== '/'){
+        niv--;
+        printspace(niv);
+        c << s << endl;
     }
-    fin.close();
-    fout.close();
-    return 0;
-}
+
+    else if (s[0] == '<' && s[strlen(s)-1] == '>')
+        if (s[strlen(s)-2] == '/'){
+        printspace(niv);
+            c << s << endl;}
+        else if ( s[1]== '!') {niv --;
+                                cout << s << endl;
+                                niv ++;}
+        else {
+            printspace(niv);
+            c << s << endl;
+            niv ++ ;
+        }
+    else { if (s[strlen(s)-1]== ';' || s[strlen(s)-1]== '}')
+
+        c << s <<endl;
+        else {printspace (niv);
+                c << s << endl;
+
+    }
+    } }
+    f.close();
+    c.close();
+    return 0;}
